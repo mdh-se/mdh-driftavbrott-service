20aca05e5ae17b4876916732e380f7469bd3ec91
Johan Nilsson <s.j.nilsson@gmail.com>
2018-09-11 15:06:16 +0200

    Optimerade importer

--- src/test/java/se/mdh/driftavbrott/rest/DriftavbrottControllerTest.java
+++ src/test/java/se/mdh/driftavbrott/rest/DriftavbrottControllerTest.java
@@ -16,7 +16,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
