0f9bdd18c8c4a2ff9ec832965f64a9d433196f1a
Johan Nilsson <s.j.nilsson@gmail.com>
2022-06-30 08:58:15 +0200

    INFRA-1654 - Uppdatera Spring Boot

---
+++ src/main/java/se/mdh/driftavbrott/configuration/DocConfig.java
@@ -0,0 +1,20 @@
+package se.mdh.driftavbrott.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by johan on 2017-02-08.
+ */
+@Configuration
+public class DocConfig {
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new Info().title("mdh-driftavbrott-service")
+                  .description("API för mdh-driftavbrott-service.")
+                  .version("v1"));
+  }
+}
--- src/main/java/se/mdh/driftavbrott/rest/DriftavbrottController.java
+++ src/main/java/se/mdh/driftavbrott/rest/DriftavbrottController.java
@@ -1,14 +1,9 @@
 package se.mdh.driftavbrott.rest;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
@@ -22,6 +17,12 @@ import se.mdh.driftavbrott.modell.Driftavbrott;
 import se.mdh.driftavbrott.repository.DriftavbrottpostRepositoryException;
 import se.mdh.driftavbrott.service.IcService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/v1/driftavbrott")
 public class DriftavbrottController {
@@ -34,15 +35,15 @@ public class DriftavbrottController {
   }
 
   @ApiResponses(value = {
-      @ApiResponse(code = 200, message = "Det finns ett driftavbrott."),
-      @ApiResponse(code = 204, message = "Det finns inget driftavbrott."),
+      @ApiResponse(responseCode = "200", description = "Det finns ett driftavbrott."),
+      @ApiResponse(responseCode = "204", description = "Det finns inget driftavbrott."),
   })
-  @ApiOperation(value = "Hämta pågående driftavbrott för ett antal kanaler",
-      notes = "Endast det driftavbrott som har den sista sluttidpunkten returneras")
+  @Operation(summary = "Hämta pågående driftavbrott för ett antal kanaler",
+      description = "Endast det driftavbrott som har den sista sluttidpunkten returneras")
   @GetMapping(value = "/pagaende", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<Driftavbrott> getPagaende(@ApiParam(value = "Kanaler att kontrollera") @RequestParam(value = "kanal", required = false) final String[] kanaler,
-                                                  @ApiParam(value = "Anropande system", required = true) @RequestParam(value = "system") final String system,
-                                                  @ApiParam(value = "Marginal i minuter") @RequestParam(value = "marginal", defaultValue = "0") final int marginalMinuter,
+  public ResponseEntity<Driftavbrott> getPagaende(@Parameter(description = "Kanaler att kontrollera") @RequestParam(value = "kanal", required = false) final String[] kanaler,
+                                                  @Parameter(description = "Anropande system", required = true) @RequestParam(value = "system") final String system,
+                                                  @Parameter(description = "Marginal i minuter") @RequestParam(value = "marginal", defaultValue = "0") final int marginalMinuter,
                                                   final HttpServletRequest request) {
     String logInfo = system + " -> " + request.getMethod() + " /pagaende med kanaler=" + Arrays.toString(kanaler) + " och marginal i minuter=" + marginalMinuter;
     log.info(logInfo);
